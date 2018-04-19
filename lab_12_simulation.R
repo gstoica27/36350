1cbac4f7f48d13bc104dea142d2154aef7b8dfc7
gstoica27 <gstoica27@gmail.com>
2018-04-19 12:08:24 -0400

    Corrected code for simuate data function

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -23,23 +23,30 @@ model_select = function(covariates, responses, cutoff) {
 
 run_simulation = function(n_trials, n, p, cutoff) {
   all_p_vals = c()
-  for (trial in 1:n_trials) {
+  
     for (num_ftrs in p) {
       for (num_obs in n) {
+        all_p_vals = c()
+        for (trial in 1:n_trials) {
         #browser()
         data = generate_data(num_obs, num_ftrs)
         p_vals = model_select(data$covariates, data$responses, cutoff = cutoff)
         all_p_vals = c(all_p_vals, p_vals)
+        
+        }
+        if(length(all_p_vals) > 0) {
+          hist(all_p_vals, xlab = "p-values", ylab = "Frequency",
+                    main = "Distribution of p-values")
+        }
       }
     }
-  }
   #print(all_p_vals)
-  if(length(all_p_vals) > 0) {
-    plot(all_p_vals, xlab = "p-values", ylab = "Frequency",
-         main = "Distribution of p-values")
-  }
+  
   
 }
 
 #run_simulation(1, c(100), c(50), .05)
-
+#n = c(100, 1000,   10000)
+#p = c(10, 20, 50)
+#cutoff = 0.05
+#run_simulation(1, n, p, cutoff)
