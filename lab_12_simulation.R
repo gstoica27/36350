c470d610cf20d5254cd43c9f21ea8ff79cb4097f
gstoica27 <gstoica27@gmail.com>
2018-04-19 11:09:22 -0400

    Added model_select function

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -3,3 +3,14 @@ generate_data = function(n, p) {
   responses = rnorm(n)
   return(list(covariates = covariates, responses = responses))
 }
+
+model_select = function(covariates, responses, cutoff) {
+  orig_lm = lm(responses ~ covariates)
+  p_vals = summary(orig_lm)$coef[-1,4]
+  p_vals_sig = p_vals <= cutoff
+  vec = c()
+  if (sum(p_vals_sig) == 0) return(vec)
+  new_lm = lm(responses ~ covariates[, p_vals_sig])
+  new_p_vals = summary(new_lm)$coef[-1,4]
+  return(new_p_vals)
+}
